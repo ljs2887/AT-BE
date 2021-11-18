dfcb2ac9a2999bd85648a9344090d94ba5d5b22f
ljs2887 <jio5402@naver.com>
2021-11-18 11:59:54 +0900

    공지사항 수정 및 삭제 완료

--- router/notice.js
+++ router/notice.js
@@ -35,14 +35,48 @@ router.get('/', async (req, res) => {
 	}
 })
 
-// router.get('/:id', async (req, res) => {
-// 	try {
-// 		const { id } = req.params
-// 		const notice = await Notice.findOne({ postId: id })
-// 	} catch (error) {
-// 		console.error(error)
-// 		return req.status(500).send(error)
-// 	}
-// })
+router.get('/:id', async (req, res) => {
+	try {
+		const { id } = req.params
+		const notice = await Notice.findOne({ postId: id })
+		return res.status(200).send(notice)
+	} catch (error) {
+		console.error(error)
+		return res.status(500).send(error)
+	}
+})
+
+router.delete('/:id', async (req, res) => {
+	try {
+		const { id } = req.params
+		await Notice.deleteOne({ postId: id })
+		return res.status(200).send('삭제성공')
+	} catch (error) {
+		console.error(error)
+		return res.status(500).send(error)
+	}
+})
+
+router.put('/:id', async (req, res) => {
+	try {
+		const { id } = req.params
+		const { title, user, content } = req.body
+		if (title === '' || user === '' || content === '') {
+			return res.status(400).send('빈 값이 있으면 안됩니다.')
+		}
+		await Notice.updateOne(
+			{ postId: id },
+			{
+				title: title,
+				user: user,
+				content: content
+			}
+		)
+		return res.status(200).send('수정 성공')
+	} catch (error) {
+		console.error(error)
+		return res.status(500).send(error)
+	}
+})
 
 export default router
